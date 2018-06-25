824c34b7789aef0991f32b6c1adbc5d55a58f508
binoyskumar92 <subhakum@usc.edu>
2018-06-24 17:27:16 -0700

    Build a basic list of books. MapStatetoProps and container covered

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function(){
+  return[
+    {title: "Javascript good parts"},
+    {title: "Harry potter"},
+    {title: "Moby dick"},
+    {title: "Agatha christie"},
+    {title: "Sherlock"},
+    {title:"Bullshit"}
+  ]
+}
