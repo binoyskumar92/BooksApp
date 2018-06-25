3cf6163554fe75b93b116593542577fd8d324c61
binoyskumar92 <subhakum@usc.edu>
2018-06-24 20:07:54 -0700

    Created action creators and wired upto onclick event of list item and info displayed in console

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-
+import {selectBook} from "../actions/index";
+import {bindActionCreators} from 'redux';
 class BookList extends Component {
 
 	renderList() {
 		return this.props.books.map((book) => {
-			return (<li key={book.title} className="list-group-item">{book.title}</li>);
+			return (<li onClick={()=>this.props.selectBook(book)} key={book.title} className="list-group-item">{book.title}</li>);
 		});
 	}
 
@@ -20,9 +21,16 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
+	//whatever is returned will show up as props in BookList
 	return {
 		books: state.books
 	}
 }
-
-export default connect(mapStateToProps)(BookList);
+//Anything returned from this function will available as props to container
+function mapDispatchToProps(dispatch){
+	//Whenever select book is called result should be passed to all of the reducers
+	return bindActionCreators({selectBook},dispatch);
+}
+//promote a BookList from a component to container. it needs to know the new dispatch method
+//selectBook. Hence make it available as a prop
+export default connect(mapStateToProps,mapDispatchToProps)(BookList);
